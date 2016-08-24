458f8a59352ed4606ecfd06f8fa7f30683c3a00e
RubyBe <benavente@outlook.com>
2016-08-24 09:26:27 -0700

    clone angular template into music project

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+// import Component code from angulary library
+import { Component } from 'angular2/core';
+
+// annotation
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class = "container">
+      <h1>Skeleton Angular2 App!</h1>
+    </div>
+  `
+})
+// class declaration
+export class AppComponent {
+
+}
